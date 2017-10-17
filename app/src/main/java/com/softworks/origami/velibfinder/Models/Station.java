fccb897d8b8603df80dbdbe14247024ae43dacf4
Franck Zhang <franck.zhang@epita.fr>
2017-10-17 19:21:41 +0200

    Remove dead code

--- app/src/main/java/com/softworks/origami/velibfinder/Models/Station.java
+++ app/src/main/java/com/softworks/origami/velibfinder/Models/Station.java
@@ -32,18 +32,6 @@ public class Station {
         }
         return newStation;
     }
-
-    //public Station(LRecords records) { this.records = records; }
-    /*
-    public Station(String name, String address, boolean open, int bike_stands, int available_bike_stands, Date lastUpdate) {
-        this.name = name;
-        this.address = address;
-        this.open = open;
-        this.bike_stands = bike_stands;
-        this.available_bike_stands = available_bike_stands;
-        this.lastUpdate = lastUpdate;
-    }
-    */
 }
 
 
